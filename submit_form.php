1752acd2de30a332b1adb7a9c967c8df033d2418
dansdive <163615062+dansdive@users.noreply.github.com>
2024-09-27 23:07:29 +0800

    Update submit_form.php

--- submit_form.php
+++ submit_form.php
@@ -3,14 +3,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $first_name = htmlspecialchars($_POST['first_name']);
     $last_name = htmlspecialchars($_POST['last_name']);
     $email = htmlspecialchars($_POST['email']);
-    $phone = htmlspecialchars($_POST['phone']);
     $message = htmlspecialchars($_POST['message']);
+    $phone = isset($_POST['phone']) ? htmlspecialchars($_POST['phone']) : '';
 
     // Here you can add code to send an email or save the data to a database
     // For example, using the mail() function to send an email:
     $to = "maryjoy.padiz1@gmail.com";
     $subject = "New Inquiry from $first_name $last_name";
-    $body = "Name: $first_name $last_name\nEmail: $email\nPhone: $phone\nMessage: $message";
+    $body = "Name: $first_name $last_name\nEmail: $email\nMessage: $message";
+    if (!empty($phone)) {
+        $body .= "\nPhone: $phone";
+    }
     $headers = "From: $email";
 
     if (mail($to, $subject, $body, $headers)) {
